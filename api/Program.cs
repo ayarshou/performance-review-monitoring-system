98895b4926db699d8bcaecb6abe17d27cde77b26
Aliaksei.Yarshou-Mazurau <a.yarshou-mazurau@godeltech.com>
2026-03-23 16:29:37 +0100

    wip

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,8 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using PerformanceReviewApi.Data;
+using PerformanceReviewApi.Repositories;
+using PerformanceReviewApi.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +26,11 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IReviewSessionRepository, ReviewSessionRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddValidatorsFromAssemblyContaining<SubmitReviewRequestValidator>();
 
 builder.Services.AddCors(options =>
 {
